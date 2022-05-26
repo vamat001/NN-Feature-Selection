6fd826a20c79e36c62d0cf5cbf8dc16cbc622930
Vivek Amatya <vamatya99@gmail.com>
2022-05-25 22:23:28 -0700

    first commit

---
+++ feature_selection.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to Vivek Amatya's Feature Selection Algorithm.")
+    file = input("Type in the name of the file to test: ")
+    algorithm = input("\nType the number of the algorithm you want to run.\n\n\t1) Forward Selection\n\t2) Backward Elimination\n")
+    # print(file)
+    # print(algorithm)
+
+if __name__ == "__main__":
+    main()
